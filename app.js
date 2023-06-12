aeb28f71c4f64265be2317adcb2df990b212cc30
Thanna1999 <nawuttt1999@gmail.com>
2023-06-12 21:04:52 +0700

    update

--- app.js
+++ app.js
@@ -155,11 +155,6 @@ app.get("/about", function(req, res) {
 });
 
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 8000;
-}
-
-app.listen(port, function() {
-  console.log("Server started successfully");
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
 });
